e1e17b6976302be7482ea7d73148406904e05ba0
biola <cipherx809@gmail.com>
2023-03-29 03:42:27 -0700

    added social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = ()=>{
         <div className={styles.footer} id="footer">
                 <div>
                     <ul className={styles.verticalList}>
-                        <li>Twitter</li>
-                        <li>Instagram</li>
-                        <li>LinkedIn</li>
+                    <li><a href="https://twitter.com/SpaceUni_Africa">Twitter</a></li>
+                    <li><a href="https://www.instagram.com/spaceuniverse.africa">Instagram</a></li>
+                    <li><a href="https://linkedin.com/company/spaceuniverse-africa">LinkedIn</a></li>
                     </ul>
                     <span>© 2023 Space Universe Africa | All rights reserved.</span>
                 </div>
